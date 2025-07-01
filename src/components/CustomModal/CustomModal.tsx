6ce9eef92c2029ece546bcc55bcc145a5fc8d38e
Bogdan Pavliv <bogdan.pavliv@gmail.com>
2025-07-01 20:04:05 +0300

    add project

--- src/components/CustomModal/CustomModal.tsx
+++ src/components/CustomModal/CustomModal.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Modal from "react-modal";
-import CloseIcon from "./close-icon.svg";
 import "./CustomModal.scss";
 import { CustomModalProps } from "../../types/others";
 
@@ -15,7 +14,10 @@ export const CustomModal: React.FC<CustomModalProps> = ({ isOpen, onClose, submi
       ariaHideApp={false}
     >
       <button className="modal-close-button" onClick={() => onClose()}>
-        <CloseIcon />
+        <svg width="31" height="30" viewBox="0 0 31 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M22.6257 22.0711L8.34218 7.92893" stroke="inherit" strokeWidth="2" strokeLinecap="round"/>
+          <path d="M22.6257 7.92893L8.34218 22.0711" stroke="inherit" strokeWidth="2" strokeLinecap="round"/>
+        </svg>
       </button>
       {children}
       {submitButton && (
